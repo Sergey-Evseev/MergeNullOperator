f02c4a970ffdc2ede4dae42d4313f0597ff9e149
Sergey Evseev <sevseev@gmail.com>
2023-04-30 17:33:37 +0300

    private List<Person> GetPersons()

---
+++ MergeNullOperator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MergeNullOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+
+            str ??= string.Empty;
+
+            Console.WriteLine($"Количество символов в строке: {str.Length}");
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ NamesOutput/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace NamesOutput
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(23, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(514, 264);
+            this.listBox1.TabIndex = 0;
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(23, 304);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(97, 30);
+            this.runButton.TabIndex = 1;
+            this.runButton.Text = "RUN";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 356);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "List Names";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button runButton;
+    }
+}
+
