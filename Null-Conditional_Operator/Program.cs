f4c75d87d4d5c2e9bb0162e4d8735234e7cd1569
Sergey Evseev <sevseev@gmail.com>
2023-04-30 19:23:58 +0300

    static int[]GetArray()

---
+++ Null-Conditional_Operator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null_Conditional_Operator
+{
+    internal class Program
+    {
+        static int[]GetArray()
+        {
+            int[] myArray = { 1, 2, 3 };
+            //int[] myArray = null;
+            return myArray;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] myArray = GetArray();
+            Console.WriteLine("Сумма элементов массива: " + (myArray?.Sum() ?? 0));
+        }
+    }  
+}
